897ef5cf6678166f46ec650b00679f3b56d2b4f3
Arnobio Fernandes <arnobiopaulinofernandes@gmail.com>
2020-03-30 23:07:01 -0300

    terceiro commit inserido table

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	
+	<table width="600" border="1">
+		<th>Nome</th>
+		<th>Email</th>
+		<th>Senha</th>
+	</table>
 
 </body>
 </html>
